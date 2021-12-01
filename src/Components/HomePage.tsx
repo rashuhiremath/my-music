dbf3785dd30a21d9c112790c3d33525f975ea39b
rashuhiremath <rashmi.hiremath94@gmail.com>
2021-12-01 16:10:59 +0100

    home work updated

---
+++ src/Components/HomePage.tsx
@@ -0,0 +1,60 @@
+import { useState, FormEvent } from "react";
+import { Form } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import Music from "./interfaces/Music";
+import { Card, Button } from "react-bootstrap";
+
+const HomePage = () => {
+   const [query, setQuery] = useState("")
+  const [music, setMusic] = useState<Music[]>([]);
+
+  const getMyMusic = async (query: string) => {
+    try {
+      let response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`
+      );
+      if (response.ok) {
+        let music = await response.json();
+       let songs = music.data
+        setMusic(songs);
+      } else {
+        console.log("couldn't fetch the music");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+    const handleSubmit = async (e: FormEvent) => {
+      e.preventDefault();
+      getMyMusic(query)
+    };
+
+//   useEffect(() => {
+//     getMyMusic();
+//   }, []);
+
+  return (
+    <Container>
+      <Row>
+        <Col xs={10} className="mx-auto my-3">
+          <h1>Music Search</h1>
+        </Col>
+          <Col xs={10} className="mx-auto">
+           <Form onSubmit={handleSubmit}>
+              <Form.Control
+                type="search"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="type and press Enter"
+              />
+            </Form> 
+        </Col>  
+        {  music.map((song) => (
+        <p>{song.title}</p>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+export default HomePage;
